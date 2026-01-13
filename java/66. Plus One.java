16812991e244aee9a5a2a1584989e220bf59cf01
Jhanani03 <jhananikabilan@gmail.com>
2026-01-13 21:09:38 +0530

    Create 66. Plus One.java

---
+++ java/66. Plus One.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int N=digits.length;
+        for(int i=N-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i]=0;
+            }
+        }
+        int[] arr=new int[N+1];
+        arr[0]=1;
+        return arr;
+        }
+    }
